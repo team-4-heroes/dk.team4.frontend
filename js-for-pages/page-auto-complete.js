da0fbdcac48391d1fc9129fc0b903b4016941e73
SirMeows <araelbrighteyes@gmail.com>
2022-03-23 15:17:20 +0100

    tidy up unnecessary print statements

--- js-for-pages/page-auto-complete.js
+++ js-for-pages/page-auto-complete.js
@@ -1,10 +1,7 @@
 
-console.log('hello world');
-
 dawaAutocomplete.dawaAutocomplete(document.getElementById('dawa-autocomplete-input'), {
   select: function(selected) {
     document.getElementById('selected-address').innerHTML = selected.tekst;
-    console.log('Valgt adresse: ' + selected.tekst);
   }
 });
 
