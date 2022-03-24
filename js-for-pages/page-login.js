2e496a883b119e1f353073c563709ecbcc66a2a5
ptsand <ptrsand@gmail.com>
2022-03-24 15:59:10 +0100

    save username in session storage

--- js-for-pages/page-login.js
+++ js-for-pages/page-login.js
@@ -9,7 +9,7 @@ function login() {
   let user = {username: document.getElementById("username").value, password: document.getElementById("password").value}
   fetch("http://localhost:8080/api/auth/login", makeOptions("POST", user)).then(resp=>resp.json())
   .then(resp=>{
-    setLoginState(resp.token, resp.roles)
+    setLoginState(resp.token, resp.roles, resp.username)
     showPage("page-home") // redirect to page-home
   })
 }
@@ -18,15 +18,15 @@ export function logout() {
   setLoginState(null)
 }
 
-export function setLoginState(token, loggedInAs) {
+export function setLoginState(token, loggedInAs, username) {
   if (token) {
     sessionStorage.setItem("token", token)
-    if (loggedInAs) {
-      sessionStorage.setItem("logged-in-as", loggedInAs)
-    }
+    if (loggedInAs) sessionStorage.setItem("logged-in-as", loggedInAs)
+    if (username) sessionStorage.setItem("username", username)
   } else {
     sessionStorage.clear("token")
     sessionStorage.clear("logged-in-as")
+    sessionStorage.clear("username")
   }
   updateLoginDependentComponents()
 }
@@ -34,20 +34,13 @@ export function setLoginState(token, loggedInAs) {
 export function updateLoginDependentComponents() {
   const loggedIn = sessionStorage.getItem("token")
   const loggedInAs = sessionStorage.getItem("logged-in-as")
-  /*document.getElementById("logged-in-user").style.display = "none"
-  document.getElementById("logged-in-admin").style.display = "none"
-  document.getElementById("not-logged-in").style.display = "block"
-  document.getElementById("user-role").innerText = "" */
-  if (loggedInAs === "ADMIN") {
-    // document.getElementById("logged-in-admin").style.display = loggedIn ? "block" : "none"
-  }
-  if (loggedInAs === "USER") {
-    // document.getElementById("logged-in-user").style.display = loggedIn ? "block" : "none"
-  }
+  const username = sessionStorage.getItem("username")
+
   if (loggedIn) {
-    //document.getElementById("not-logged-in").style.display = "none"
-    //document.getElementById("user-role").innerText = "Logged in as: " + loggedInAs
+    document.getElementById("user-details").innerText = `Logged on with username ${username} as ${loggedInAs}`
+    document.getElementById("user-details").style.display = "inline"
   }
   document.getElementById("page-login").style.display = loggedIn ? "none" : "block"
   document.getElementById("page-logout").style.display = loggedIn ? "block" : "none"
+  document.getElementById("user-details").style.display = loggedIn ? "inline" : "none"
 }
