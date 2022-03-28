a5d12f54e8999ccdbcba26a7208facdf6eef0c3c
Christian Hundahl <chri02jx@stud.kea.dk>
2022-03-28 09:36:00 +0200

    Switched to online db and static web app

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = "http://localhost:8080/api/"
+//export const SERVER_URL = "http://localhost:8080/api/"
 
-//export const SERVER_URL = "https://krakproject.azurewebsites.net/api/"
+export const SERVER_URL = "https://krakproject.azurewebsites.net/api/"
